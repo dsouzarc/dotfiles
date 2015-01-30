8dc82c802f1de71dc89c5c260ad454f2103be252
Ryan D'souza <dsouzarc@gmail.com>
2015-01-30 17:35:50 -0500

    Modified version

--- TextPhone.py
+++ TextPhone.py
@@ -9,12 +9,22 @@ message = sendgrid.Mail();
 message.add_to("Ryan D'souza <" + config.PHONE + "@vtext.com>");
 message.set_from("Ryan D'souza <Ryans Macbook Pro Command Line>");
 
-message.set_subject(sys.argv[1]);
+#If there is only one parameter, the subject is blank
+if len(sys.argv) == 2:
+    message.set_subject(" ");
+    message.set_text(sys.argv[1]);
 
-if len(sys.argv) > 2:
-    message.set_text(sys.argv[2]);
+#But, if there's also a subject
+if len(sys.argv) == 3:
+    message.set_subject(sys.argv[1]);
+    message.set_subject(sys.argv[2]);
 
 status, msg = sg.send(message);
 
-print status;
-print msg;
+#Print error
+if msg.find("success") == -1:
+    print msg;
+
+#Print success
+else:
+    print "Successfully sent"
