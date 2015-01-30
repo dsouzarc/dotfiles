7f5b6ba43adb3ccc878cf46dd548254fe8f96f9a
Ryan D'souza <dsouzarc@gmail.com>
2015-01-30 17:28:57 -0500

    Working

--- TextPhone.py
+++ TextPhone.py
@@ -1,3 +1,20 @@
-import config
+import config;
+import sendgrid;
+import sys;
 
-print config.SG_USERNAME
+sg = sendgrid.SendGridClient(config.SG_USERNAME, config.SG_PASSWORD);
+
+message = sendgrid.Mail();
+
+message.add_to("Ryan D'souza <" + config.PHONE + "@vtext.com>");
+message.set_from("Ryan D'souza <Ryans Macbook Pro Command Line>");
+
+message.set_subject(sys.argv[1]);
+
+if len(sys.argv) > 2:
+    message.set_text(sys.argv[2]);
+
+status, msg = sg.send(message);
+
+print status;
+print msg;
