f563ed16a932d0addf68e6e81f25366c29444422
Ryan D'souza <dsouzarc@gmail.com>
2015-02-10 16:58:56 -0500

    Creates a new directory labeled 'AppIcon' and saves all photos to it

--- iOSAppIconResizer.py
+++ iOSAppIconResizer.py
@@ -1,11 +1,15 @@
 from PIL import Image;
+import os;
 import sys;
 
 global originalImage;
+global saveToPath;
 
 def resizeImage(newWidth, newHeight, imageDescription):
     newImage = originalImage.resize((newWidth, newHeight), Image.ANTIALIAS);
-    newImage.save("Icon-" + imageDescription + ".png", quality=100);
+    
+    newFileName = saveToPath + "/Icon-" + imageDescription + ".png";
+    newImage.save(newFileName, quality=100);
 
 if len(sys.argv) == 1:
     fileName = raw_input("Enter file name and directory:\n");
@@ -15,4 +19,11 @@ else:
 
 originalImage = Image.open(fileName);
 
+pathWhenExecuting = os.getcwd();
+saveToPath = pathWhenExecuting + "/AppIcon";
+
+if not os.path.exists(saveToPath):
+    os.makedirs(saveToPath);
+
 resizeImage(180, 180, "Small");
+
