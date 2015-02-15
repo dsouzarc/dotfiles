865b14d2de9e3bb5370c8c403a86533765c6b573
Ryan D'souza <dsouzarc@gmail.com>
2015-02-14 23:48:03 -0500

    Uses po.st to shorten URL

--- Bash/ShortenURL.sh
+++ Bash/ShortenURL.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-JSONResult=$(python2 -c "import urllib; print urllib.urlopen(\"http://po.st/api/shorten?longUrl=facebook.com&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547\",'r').read()")
+JSONResult=$(python2 -c "import urllib; print urllib.urlopen(\"http://po.st/api/shorten?longUrl=$1&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547\",'r').read()")
 
 echo $JSONResult
