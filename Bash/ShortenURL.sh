c1a1dec0a09b678d57d68208837a013dad862fb1
Ryan D'souza <dsouzarc@gmail.com>
2015-02-14 23:55:06 -0500

    Final changes

--- Bash/ShortenURL.sh
+++ Bash/ShortenURL.sh
@@ -4,7 +4,11 @@
 JSONResult=$(python2 -c "import urllib; print urllib.urlopen(\"http://po.st/api/shorten?longUrl=$1&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547\",'r').read()")
 
 #Parses result to see if it was successful
-Success=$(node -pe 'JSON.parse(process.argv[1]).name' "$JSONResult")
+Success=$(node -pe 'JSON.parse(process.argv[1]).status_txt' "$JSONResult")
+
+if [ "$Success" == "OK" ] then
+    echo YES
+fi
 
 echo $Success
 
