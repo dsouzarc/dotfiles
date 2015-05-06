988c3c4c9ad76e33a3e2a1def3ec57b594a74c7f
Ryan D'souza <dsouzarc@gmail.com>
2015-05-06 15:27:12 -0400

    Bug fixes

--- Python/CredentialManager.py
+++ Python/CredentialManager.py
@@ -3,8 +3,7 @@ import json;
 import getpass; 
 import os;
 
-directory_path = str(os.path.expanduser("~")) + "/.my_credential_manager";
-
+directory_path = str(os.path.expanduser("~")) + "/.my_credential_manager/";
 file_path = directory_path + ".credential_files.json";
 
 if not os.path.exists(directory_path):
@@ -13,14 +12,43 @@ if not os.path.exists(directory_path):
 
 if os.path.isfile(file_path):
     try:
-        credentials = json.load(file_path);
-        print("Credentials loaded");
-    except Error:
-        print("Problem loading existing credentials from file. Building new file");
+        with open(file_path) as credential_file:
+            credentials = json.load(credential_file);
+            print(credentials);
+            print("Credentials loaded");
+    except ValueError:
+        print("Value error for loading existing credentials from file. Building new file");
         credentials = {};
 else:
     print("No existing credentials");
     credentials = {};
+
+def save_credentials(usernameKey, username, password, passwordKey):
+    
+    if usernameKey in credentials:
+        print("Username already exists for '" + usernameKey + "'.");
+
+        answer = raw_input("Override? y/n: ");
+
+        if answer == 'y':
+            credentials[usernameKey] = base64.b64encode(username);
+            credentials[passwordKey] = base64.b64encode(password);
+
+            with open(file_path, 'w') as file:
+                json.dump(credentials, file, sort_keys = True, indent = 4, ensure_ascii = False);
+                print("Credentials saved");
+                return;
+        else:
+            print("Cancelled");
+            return;
+    else:
+        credentials[usernameKey] = base64.b64encode(username);
+        credentials[passwordKey] = base64.b64encode(password);
+        
+        with open(file_path, 'w') as file:
+            json.dump(credentials, file, sort_keys = True, indent = 4, ensure_ascii = False);
+            print("Credentials saved");
+            return;
 
 usernameKey = raw_input("Username key: ");
 username = raw_input("Username: ");
@@ -28,4 +56,5 @@ username = raw_input("Username: ");
 passwordKey = raw_input("Password key: ");
 password = getpass.getpass("Password: ");
 
-print(password);
+save_credentials(usernameKey, username, password, passwordKey);
+
