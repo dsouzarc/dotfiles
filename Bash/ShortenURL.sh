1fa77e88ee835451d2543b25742d973144a465d1
Ryan D'souza <dsouzarc@gmail.com>
2015-06-18 14:19:35 -0400

    #!/bin/bash in beginning

--- Bash/ShortenURL.sh
+++ Bash/ShortenURL.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 #Sends a request to the po.st API with the long url. po.st returns JSON with result information
 JSONResult=$(python2 -c "import urllib; print urllib.urlopen(\"http://po.st/api/shorten?longUrl=$1&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547\",'r').read()")
