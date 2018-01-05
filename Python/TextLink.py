19301b8b3f76fbd4e0b214143bc03a77fb837eff
Ryan D'souza <dsouzarc@gmail.com>
2018-01-05 02:20:47 -0500

    Optimized to use new version of the CredentialManager

--- Python/TextLink.py
+++ Python/TextLink.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import sys;
-import urllib2;
-import urllib;
+import sys
+import urllib2
+import urllib
 import json
-import CredentialManager;
 
 from pprint import pprint
+from CredentialManager import CredentialManager
 
 #######################################################################
 # Written by Ryan D'souza
@@ -22,32 +22,31 @@ from pprint import pprint
 #######################################################################
 
 def compress_url(url):
-        JSON = json.loads(urllib.urlopen("http://po.st/api/shorten?longUrl=" + url + "&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547",'r').read());
-        return JSON['short_url'];
+        JSON = json.loads(urllib.urlopen("http://po.st/api/shorten?longUrl=" + url + "&apiKey=3E5C05F5-DDED-4485-A193-F486E947F547",'r').read())
+        return JSON['short_url']
 
-url = "https://api.sendgrid.com/api/mail.send.json";
-to = CredentialManager.get_value("PHONE") + "@vtext.com";
-from_ = "MBProCL";
-username = CredentialManager.get_value("SendGridUsername");
-password = CredentialManager.get_value("SendGridPassword");
+url = "https://api.sendgrid.com/api/mail.send.json"
+to = CredentialManager.get_value("PHONE") + "@vtext.com"
+from_ = "MBProCL"
+username, password = CredentialManager().get_account('SendGrid')
 
 #If a link has been included as a parameter
 if len(sys.argv) == 2:
 
     #If the link URL is less than 150 chars
     if len(sys.argv[1]) <= 150:
-        bodyText = sys.argv[1];
+        bodyText = sys.argv[1]
 
     #Else, compress it
     else:
-        bodyText = compress_url(sys.argv[1]);
+        bodyText = compress_url(sys.argv[1])
 
 #Otherwise, just prompt for link
 else:
-    bodyText = raw_input("Enter text content: ");
+    bodyText = raw_input("Enter text content: ")
 
     if len(bodyText) >= 150:
-        bodyText = compress_url(bodyText);
+        bodyText = compress_url(bodyText)
 
 params = {
     "api_user": username,
@@ -56,14 +55,14 @@ params = {
     "to": to,
     "subject": " ",
     "text": bodyText
-};
+}
 
-params = urllib.urlencode(params);
+params = urllib.urlencode(params)
 
-request = urllib.urlopen(url, params);
-response = request.read();
+request = urllib.urlopen(url, params)
+response = request.read()
 
 if response.find("success") == -1:
-    print(response);
+    print(response)
 else:
-    print("Successfully sent");
+    print("Successfully sent")
